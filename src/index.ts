1eda9a33af2882b83a67154ea3f6b82f4ccba2f3
sasha <kredich05@mail.ru>
2025-02-21 22:05:22 +0300

    add publicClient and funcs

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,33 @@
-console.log("Hello via Bun!");
+import abi from "./abi.json"
+import { createPublicClient, http } from 'viem'
+import { mainnet } from 'viem/chains'
+
+
+require('dotenv').config();
+
+const privateKey = process.env.PRIVATE_KEY;
+
+const RPC_URL = process.env.RPC_URL;
+
+const publicClient = createPublicClient({
+  chain: mainnet,
+  transport: http(RPC_URL)
+})
+
+
+const totalSupply = await publicClient.readContract({
+  address: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+  abi: abi,
+  functionName: 'totalSupply',
+  args: []
+})
+
+const decimals = await publicClient.readContract({
+  address: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+  abi: abi,
+  functionName: 'decimals',
+  args: []
+})
+
+
+
